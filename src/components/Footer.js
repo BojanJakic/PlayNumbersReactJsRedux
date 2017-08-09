4517ecf5870cf1ea810c3ad0f4792276d0061ac8
BojanJakic <bojanjakic1@gmail.com>
2017-08-09 12:14:46 +0200

    Open link in new tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 footer-container">
             <hr/>
-            <a href="">Github</a>
+            <a href="https://github.com/BojanJakic/PlayNumbersReactJsRedux" target="_blank" rel="noopener noreferrer">Github Source Code</a>
         </div>
     )
 };
