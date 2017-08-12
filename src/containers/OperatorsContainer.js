4fe1a807c78abf4cd44c33a52ab61a639637ec6f
BojanJakic <bojanjakic1@gmail.com>
2017-08-12 18:04:23 +0200

    Validate clicked operator before dispatch the action

--- src/containers/OperatorsContainer.js
+++ src/containers/OperatorsContainer.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react';
 import { connect } from 'react-redux';
 import Operators from '../components/Operators';
+import { isInputValid } from '../js/validator.js';
 
 class OperatorsContainer extends Component{
     render = () => {
@@ -10,24 +11,31 @@ class OperatorsContainer extends Component{
                 {
                     this.props.operatorsValue.operators.map((current, index) => {
                         return(
-                            <Operators clickHandler={this.props.onOperatorClickHandler} disabled={current.isDisabled} key={index} value={current.value}/>
+                            <Operators clickHandler={this.onOperatorClickHandler} disabled={current.isDisabled} key={index} value={current.value}/>
                         )
                     })
                 }
             </div>
         )
+    };
+
+    onOperatorClickHandler = value => {
+        if(isInputValid(value, this.props.input)) {
+            this.props.dispatchOperatorAction(value)
+        }
     }
 }
 
 const mapStateToProps = (state) => {
     return {
-        operatorsValue : state.operatorsReducer
+        operatorsValue : state.operatorsReducer,
+        input : state.usersInputReducer.inputValue
     }
 };
 
 const mapDispatchToProps =(dispatch) => {
     return {
-        onOperatorClickHandler : value => dispatch({type : 'ADD_OPERATOR', payload : value})
+        dispatchOperatorAction : value => dispatch({type : 'ADD_OPERATOR', payload : value})
     }
 };
 
